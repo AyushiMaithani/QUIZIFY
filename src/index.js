58b6cec8a5603a83501fee598148eabbf2a81c07
Ayushi Maithani <ayushimaithani04@gmail.com>
2024-06-09 17:03:25 +0530

    add login and signup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+//use ejs as view engine
+app.set("view engine", "ejs");
+//adds public automatically while linking css file
+app.use(express.static("public"));
+
+//base route
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
